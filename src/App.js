1982d7aea2d380cd44f57a5d75d19218c13d6775
Mohamed Moncef EL ATLASSY <mohamed.moncef.elatlassy@gmail.com>
2022-01-16 18:26:20 +0100

    Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Brand, Navbar, CTA } from './components'
 import { Blog, Features, Header, Footer, Possibility, WhatGPT3 } from './containers'
+import './App.css';
 
 const App = () => {
   return (
